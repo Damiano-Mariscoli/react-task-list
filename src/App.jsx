e97eedfd94edd9090d301d43a287927b189f48b1
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-26 17:09:12 +0100

    ADDED: cartella tasks con la lista delle task

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,25 @@
 import "./App.css";
-
+import { tasks } from "./assets/tasks";
 function App() {
-  const tasks = [
-    {
-      id: 1,
-      title: "Implementare la homepage",
-      priority: 1,
-      estimatedTime: 120,
-      state: "completed",
-    },
-    {
-      id: 2,
-      title: "Sviluppare il menu di navigazione",
-      priority: 2,
-      estimatedTime: 60,
-      state: "completed",
-    },
-    {
-      id: 3,
-      title: "Creare il footer del sito",
-      priority: 3,
-      estimatedTime: 30,
-      state: "completed",
-    },
-    {
-      id: 4,
-      title: "Ottimizzare le performance",
-      priority: 1,
-      estimatedTime: 180,
-      state: "completed",
-    },
-    {
-      id: 5,
-      title: "Scrivere test per i componenti",
-      priority: 2,
-      estimatedTime: 90,
-      state: "completed",
-    },
-    {
-      id: 6,
-      title: "Implementare la pagina dei contatti",
-      priority: 3,
-      estimatedTime: 60,
-      state: "completed",
-    },
-    {
-      id: 7,
-      title: "Aggiungere animazioni CSS",
-      priority: 2,
-      estimatedTime: 30,
-      state: "backlog",
-    },
-    {
-      id: 8,
-      title: "Integrare l'API di autenticazione",
-      priority: 1,
-      estimatedTime: 120,
-      state: "in_progress",
-    },
-    {
-      id: 9,
-      title: "Aggiornare la documentazione",
-      priority: 3,
-      estimatedTime: 60,
-      state: "backlog",
-    },
-    {
-      id: 10,
-      title: "Rifattorizzare il codice CSS",
-      priority: 2,
-      estimatedTime: 90,
-      state: "in_progress",
-    },
-  ];
-
   return (
     <>
       <header className="header-bg">
         <h1>Task Manager</h1>
       </header>
 
-      <main className="bg-primary">
+      <main>
         {tasks
           .filter((task) => [task.state].includes("completed"))
           .map((completedTasks) => (
-            <div className="mb-5 d-flex" key={completedTasks.id}>
-              <ul>
-                <li className="bold">{completedTasks.title}</li>
-                <li>Priority:{completedTasks.priority}</li>
-                <li>Est. time {completedTasks.estimatedTime}</li>
-              </ul>
-              <button>Completed</button>
+            <div className="mb-5" key={completedTasks.id}>
+              <div>
+                <ul>
+                  <li className="bold">{completedTasks.title}</li>
+                  <li>Priority:{completedTasks.priority}</li>
+                  <li>Est. time {completedTasks.estimatedTime}</li>
+                </ul>
+                <button>Completed</button>
+              </div>
             </div>
           ))}
       </main>
