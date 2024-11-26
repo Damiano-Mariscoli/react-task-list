b71c8c789ee3ab42838d33e4399f0dc2bb6c5b29
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-26 16:28:29 +0100

    ADDED: file principali ,dependencies, e struttura base del codice con filter delle task

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const tasks = [
+    {
+      id: 1,
+      title: "Implementare la homepage",
+      priority: 1,
+      estimatedTime: 120,
+      state: "completed",
+    },
+    {
+      id: 2,
+      title: "Sviluppare il menu di navigazione",
+      priority: 2,
+      estimatedTime: 60,
+      state: "completed",
+    },
+    {
+      id: 3,
+      title: "Creare il footer del sito",
+      priority: 3,
+      estimatedTime: 30,
+      state: "completed",
+    },
+    {
+      id: 4,
+      title: "Ottimizzare le performance",
+      priority: 1,
+      estimatedTime: 180,
+      state: "completed",
+    },
+    {
+      id: 5,
+      title: "Scrivere test per i componenti",
+      priority: 2,
+      estimatedTime: 90,
+      state: "completed",
+    },
+    {
+      id: 6,
+      title: "Implementare la pagina dei contatti",
+      priority: 3,
+      estimatedTime: 60,
+      state: "completed",
+    },
+    {
+      id: 7,
+      title: "Aggiungere animazioni CSS",
+      priority: 2,
+      estimatedTime: 30,
+      state: "backlog",
+    },
+    {
+      id: 8,
+      title: "Integrare l'API di autenticazione",
+      priority: 1,
+      estimatedTime: 120,
+      state: "in_progress",
+    },
+    {
+      id: 9,
+      title: "Aggiornare la documentazione",
+      priority: 3,
+      estimatedTime: 60,
+      state: "backlog",
+    },
+    {
+      id: 10,
+      title: "Rifattorizzare il codice CSS",
+      priority: 2,
+      estimatedTime: 90,
+      state: "in_progress",
+    },
+  ];
+
+  return (
+    <>
+      <header className="header-bg">
+        <h1>Task Manager</h1>
+      </header>
+
+      <main className="bg-primary">
+        <ul>
+          {tasks
+            .filter((task) => [task.state].includes("completed"))
+            .map((completedTasks) => (
+              <li key={completedTasks.id}>{completedTasks.title}</li>
+            ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
+export default App;
